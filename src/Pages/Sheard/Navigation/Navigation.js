4fbea192fc9e44158db3bd1cff8388b74b7e9fcf
sanaul01 <mdsanaulislam154@gmail.com>
2021-11-25 20:52:17 +0600

    bike lovers added

--- src/Pages/Sheard/Navigation/Navigation.js
+++ src/Pages/Sheard/Navigation/Navigation.js
@@ -25,7 +25,7 @@ const Navigation = () => {
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    BIKE~LOVER
+                    BIKE~LOVERS
                 </Typography>
                 <NavLink to='/home' style={{textDecoration: 'none', color: 'white'}}>
                     <Button color="inherit">Home</Button>
